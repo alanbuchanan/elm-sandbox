20005ae61e8577b91c717fdcdc5f9da6c351bc9b
Rory <rory88@gmail.com>
2016-09-04 20:28:28 +0100

    Update view with methods for clarity

--- GithubUserLookup.elm
+++ GithubUserLookup.elm
@@ -42,33 +42,53 @@ update msg model =
 -- VIEW
 
 
+positionStyle : Html.Attribute Msg
+positionStyle =
+    style
+        [ ( "display", "flex" )
+        , ( "justify-content", "center" )
+        , ( "align-items", "center" )
+        , ( "flex-direction", "column" )
+        ]
+
+
+inputStyle : Html.Attribute Msg
+inputStyle =
+    style
+        [ ( "font-size", "30px" )
+        , ( "padding", "10px" )
+        , ( "width", "600px" )
+        ]
+
+
+imageStyle : Html.Attribute Msg
+imageStyle =
+    style [ ( "max-width", "200px" ) ]
+
+
+gitHubStr : String -> String
+gitHubStr user =
+    "https://github.com/" ++ user
+
+
 view : Model -> Html Msg
 view model =
     div
-        [ style
-            [ ( "display", "flex" )
-            , ( "justify-content", "center" )
-            , ( "align-items", "center" )
-            , ( "flex-direction", "column" )
-            ]
+        [ positionStyle
         ]
         [ input
-            [ placeholder "Search for a Github username"
+            [ placeholder "Search for a Github user..."
             , onInput Change
-            , style
-                [ ( "font-size", "30px" )
-                , ( "padding", "10px" )
-                , ( "width", "600px" )
-                ]
+            , inputStyle
             ]
             []
         , div []
             [ h5 [] [ text model.username ]
             ]
-        , a [ href ("https://github.com/" ++ model.username) ]
+        , a [ href (gitHubStr model.username) ]
             [ img
-                [ src ("https://github.com/" ++ model.username ++ ".png")
-                , style [ ( "max-width", "200px" ) ]
+                [ src (gitHubStr model.username ++ ".png")
+                , imageStyle
                 ]
                 []
             ]
